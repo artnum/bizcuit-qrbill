ecc43d1fd0236fe679a6590a66be228d0c2c8c4f
Etienne Bagnoud <etienne@artisan-numerique.ch>
2023-09-15 15:52:19 +0200

    Initial commit

---
+++ qrstd.php
@@ -0,0 +1,230 @@
+<?php
+/* QR Standard according to SIX Group, publishing implementation guideline for
+ * the Swiss QR Bill. Follows version 2.2. 
+ * https://www.six-group.com/dam/download/banking-services/standardization/qr-bill/ig-qr-bill-v2.2-fr.pdf
+ */
+$SWISS_QRSTD = [
+    '0200' => [
+        '_RESERVED' => [11, 12, 13, 14, 15, 16, 17],
+        'IBAN' => ['line' => 3],
+        'ADDR_CREDITOR_TYPE' => ['line' => 4],
+        'ADDR_CREDITOR_NAME' => ['line' => 5],
+        'ADDR_CREDITOR_STREET_OR_LINE1' => ['line' => 6],
+        'ADDR_CREDITOR_HOUSE_OR_LINE2' => ['line' => 7],
+        'ADDR_CREDITOR_NPA' => ['line' => 8],
+        'ADDR_CREDITOR_CITY' => ['line' => 9],
+        'ADDR_CREDITOR_COUNTRY' => ['line' => 10],
+        'AMOUNT' => ['line' => 18],
+        'CURRENCY' => ['line' => 19],
+        'ADDR_DEBITOR_TYPE' => ['line' => 20],
+        'ADDR_DEBITOR_NAME' => ['line' => 21],
+        'ADDR_DEBITOR_STREET_OR_LINE1' => ['line' => 22],
+        'ADDR_DEBITOR_HOUSE_OR_LINE2' => ['line' => 23],
+        'ADDR_DEBITOR_NPA' => ['line' => 24],
+        'ADDR_DEBITOR_CITY' => ['line' => 25],
+        'ADDR_DEBITOR_COUNTRY' => ['line' => 26],
+        'REFERENCE_TYPE' => ['line' => 27],
+        'REFERENCE' => ['line' => 28],
+        'COMMUNICATION' => ['line' => 29],
+        'EPD' => ['line' => 30],
+        'ADDITIONNAL_INFO' => ['line' => 31],
+        'PARAMS1' => ['line' => 32],
+        'PARAMS2' => ['line' => 33]
+    ]
+];
+
+/* thanks to https://commons.apache.org/proper/commons-validator/apidocs/src-html/org/apache/commons/validator/routines/checkdigit/IBANCheckDigit.html */
+function iso7064mod97_10 ($ref) {
+    $LETTER_TO_NUMBER = [
+        '0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+        '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9,
+        'a' => 10, 'b' => 11, 'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15, 
+        'g' => 16, 'h' => 17, 'i' => 18, 'j' => 19, 'k' => 20, 'l' => 21,
+        'm' => 22, 'n' => 23, 'o' => 24, 'p' => 25, 'q' => 26, 'r' => 27,
+        's' => 28, 't' => 29, 'u' => 30, 'v' => 31, 'w' => 32, 'x' => 33,
+        'y' => 34, 'z' => 35, 'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13,
+        'E' => 14, 'F' => 15, 'G' => 16, 'H' => 17, 'I' => 18, 'J' => 19,
+        'K' => 20, 'L' => 21, 'M' => 22, 'N' => 23, 'O' => 24, 'P' => 25,
+        'Q' => 26, 'R' => 27, 'S' => 28, 'T' => 29, 'U' => 30, 'V' => 31,
+        'W' => 32, 'X' => 33, 'Y' => 34, 'Z' => 35
+    ];
+    $total = 0;
+    for ($i = 0; $i < strlen($ref); $i++) {
+        $value = 0;
+        if (isset($LETTER_TO_NUMBER[$ref[$i]])) { $value = $LETTER_TO_NUMBER[$ref[$i]]; }
+        $total = ($value > 9 ? $total * 100 : $total * 10) + $value;
+        if ($total > 999999999) {
+            $total = $total % 97;
+        }
+    }
+    return $total % 97;
+}
+
+function swissMod10($ref) {
+    $bvr_table = [
+        [0, 9, 4, 6, 8, 2, 7, 1, 3, 5],
+        [9, 4, 6, 8, 2, 7, 1, 3, 5, 0],
+        [4, 6, 8, 2, 7, 1, 3, 5, 0, 9],
+        [6, 8, 2, 7, 1, 3, 5, 0, 9, 4],
+        [8, 2, 7, 1, 3, 5, 0, 9, 4, 6],
+        [2, 7, 1, 3, 5, 0, 9, 4, 6, 8],
+        [7, 1, 3, 5, 0, 9, 4, 6, 8, 2],
+        [1, 3, 5, 0, 9, 4, 6, 8, 2, 7],
+        [3, 5, 0, 9, 4, 6, 8, 2, 7, 1],
+        [5, 0, 9, 4, 6, 8, 2, 7, 1, 3]
+    ];
+
+    $r = 0;
+    //$ref = strrev($ref);
+    for ($i = 0; $i < strlen($ref); $i++) {
+        $r = $bvr_table[$r][intval($ref[$i])];
+    }
+    return [0, 9, 8, 7, 6, 5, 4, 3, 2, 1][$r];
+}
+
+function iban_verify (string $iban) {
+    return iso7064mod97_10(substr($iban, 4) . substr($iban, 0, 4)) === 1;
+}
+
+/* creditor reference starts with RF and can be anything */
+function creditorref_verify (string $reference) {
+    return iso7064mod97_10(substr($reference, 4) . substr($reference, 0, 4)) === 1;
+}
+
+/* reference is a swiss specific code that part is given by the bank and the
+ * the remaining space is free to use by the user. It use a modulo 10 algorithm.
+ */
+function reference_verify (string $reference) {
+    if (!ctype_digit($reference)) { return false; }
+    $checksum = intval(substr($reference, -1, 1));
+    $reference = substr($reference, 0, strlen($reference) - 1);
+    return swissMod10($reference) === $checksum;
+}
+
+function verify_qrdata (&$qrarray) {
+    global $SWISS_QRSTD;
+
+    if ($qrarray[0] !== 'SPC') { return false; }
+    $std = $SWISS_QRSTD[$qrarray[1]];
+    if (!isset($std)) { return false; }
+
+    /* make all upper case for easier comparison */
+    $qrarray[$std['REFERENCE_TYPE']['line']] = strtoupper($qrarray[$std['REFERENCE_TYPE']['line']]);
+    $qrarray[$std['CURRENCY']['line']] = strtoupper($qrarray[$std['CURRENCY']['line']]);
+    $qrarray[$std['ADDR_CREDITOR_COUNTRY']['line']] = strtoupper($qrarray[$std['ADDR_CREDITOR_COUNTRY']['line']]);
+    $qrarray[$std['ADDR_DEBITOR_COUNTRY']['line']] = strtoupper($qrarray[$std['ADDR_DEBITOR_COUNTRY']['line']]);
+
+    if ($qrarray[$std['EPD']['line']] !== 'EPD') { echo "EPD FALSE\n"; return false; }
+
+    /* RESERVED FOR FUTURE USE, MUST BE EMPTY */
+    foreach($std['_RESERVED'] as $reserved) {
+        if (!empty($qrarray[$reserved])) { return false; }
+    }
+
+    /* MANDATORY FIELDS */
+    if (empty($qrarray[$std['IBAN']['line']])) { return false; }
+    if (empty($qrarray[$std['CURRENCY']['line']])) { return false; }
+    if (empty($qrarray[$std['ADDR_CREDITOR_TYPE']['line']])) { return false; }
+    if (empty($qrarray[$std['ADDR_DEBITOR_TYPE']['line']])) { return false; }
+    if (empty($qrarray[$std['ADDR_CREDITOR_NAME']['line']])) { return false; }
+    if (empty($qrarray[$std['ADDR_DEBITOR_NAME']['line']])) { return false; }
+    if (empty($qrarray[$std['ADDR_CREDITOR_COUNTRY']['line']])) { return false; }
+    if (empty($qrarray[$std['ADDR_DEBITOR_COUNTRY']['line']])) { return false; }
+
+    /* verify IBAN checksum */
+    if (iban_verify($qrarray[$std['IBAN']['line']]) === false) { echo "IBAN FALSE\n"; return false; }
+
+    switch($qrarray[$std['REFERENCE_TYPE']['line']]) {
+        case 'SCOR':
+            $len = strlen($qrarray[$std['REFERENCE']['line']]);
+            if ($len < 5 || $len > 25) { return false; }
+            if (creditorref_verify($qrarray[$std['REFERENCE']['line']]) === false) { return false; }
+            break;
+        case 'QRR':
+            /* For QRR, the bank identifier must start with 3 as per swissqr documentation */
+            if (substr($qrarray[$std['IBAN']['line']], 4, 1) !== '3') { return false; }
+            /* QRR is only for CHF and EUR */
+            if (
+                $qrarray[$std['CURRENCY']['line']] !== 'CHF'
+                && $qrarray[$std['CURRENCY']['line']] !== 'EUR'
+            ) { return false; }
+            if (strlen($qrarray[$std['REFERENCE']['line']]) !== 27) { return false; }
+            if (reference_verify($qrarray[$std['REFERENCE']['line']]) === false) { return false; }
+            break;
+        case 'NON': 
+            if (!empty($qrarray[$std['REFERENCE']['line']])) { return false; }
+            break;
+        default: return false;
+    }
+
+    foreach(['ADDR_CREDITOR', 'ADDR_DEBITOR'] as $addrs) {
+        if (strlen($qrarray[$std[$addrs . '_COUNTRY']['line']]) !== 2) { return false; } 
+        switch($qrarray[$std[$addrs . '_TYPE']['line']]) {
+            case 'S':
+                break;
+            case 'K':
+                if (!empty($qrarray[$std[$addrs . '_NPA']['line']])) { return false; }
+                if (!empty($qrarray[$std[$addrs . '_CITY']['line']])) { return false; }
+                if (empty($qrarray[$std[$addrs . '_HOUSE_OR_LINE2']['line']])) { return false; }
+                break;
+        }
+    }
+
+    if (!empty($qrarray[$std['ADDITIONNAL_INFO']['line']]) && strlen($qrarray[$std['ADDITIONNAL_INFO']['line']]) > 140) { return false; }
+    if (!empty($qrarray[$std['COMMUNICATION']['line']]) && strlen($qrarray[$std['COMMUNICATION']['line']]) > 140) { return false; }
+
+    return true;
+}
+
+function bexio_from_qrdata ($qrarray) {
+    global $SWISS_QRSTD;
+
+    $std = $SWISS_QRSTD[$qrarray[1]];
+    if (!isset($std)) { return false; }
+
+    $object = new stdClass();
+
+    $object->instructed_amount = new stdClass();
+    $object->instructed_amount->currency = $qrarray[$std['CURRENCY']['line']];
+    $object->instructed_amount->amount = $qrarray[$std['AMOUNT']['line']];
+
+    /* here is the fun, Bexio seems to have not followd the guideline for Swiss
+     * qr bills (which is funny for a company that pretend to specialist ont
+     * this particular matter). So, because of that, you have to invent a street
+     * address for company that generate bills with K address line 1 empty in 
+     * order to have Bexio accept the bill ... line 2 must be splitted into two 
+     * parts to have Bexio accept the bill.
+     */
+    $object->recipient = new stdClass();
+    $object->recipient->name = $qrarray[$std['ADDR_CREDITOR_NAME']['line']];
+    $object->recipient->country_code = $qrarray[$std['ADDR_CREDITOR_COUNTRY']['line']];
+
+    if ($qrarray[$std['ADDR_CREDITOR_TYPE']['line']] === 'K') {
+        $object->recipient->street = $qrarray[$std['ADDR_CREDITOR_STREET_OR_LINE1']['line']];
+        if (empty($object->recipient->street)) { $object->recipient->street = ' '; }
+        $l2parts = explode(' ', $qrarray[$std['ADDR_CREDITOR_HOUSE_OR_LINE2']['line']], 2);
+        $object->recipient->zip = trim(array_shift($l2parts));
+        $object->recipient->city = trim(array_shift($l2parts));
+    } else {
+        $object->recipient->house_number = $qrarray[$std['ADDR_CREDITOR_HOUSE_OR_LINE2']['line']];
+        $object->recipient->street = $qrarray[$std['ADDR_CREDITOR_STREET_OR_LINE1']['line']];
+        $object->recipient->zip = $qrarray[$std['ADDR_CREDITOR_NPA']['line']];
+        $object->recipient->city = $qrarray[$std['ADDR_CREDITOR_CITY']['line']];
+    }
+    $object->iban = $qrarray[$std['IBAN']['line']];
+
+    switch($qrarray[$std['REFERENCE_TYPE']['line']]) {
+        case 'QRR':
+        case 'SCOR':
+            $object->qr_reference_nr = $qrarray[$std['REFERENCE']['line']];
+            break;
+        case 'NON':
+            $object->message = $qrarray[$std['COMMUNICATION']['line']];
+            break;
+    }
+
+    if (isset($qrarray[$std['ADDITIONNAL_INFO']['line']])) {
+        $object->additional_information = $qrarray[$std['ADDITIONNAL_INFO']['line']];
+    }
+    return $object;
+}
---
+++ read-qr.php
@@ -0,0 +1,84 @@
+<?php
+/* 
+ * Read QR for SwissQR-Bill
+ */
+
+require('qrstd.php');
+
+/* qrscanner: https://www.npmjs.com/package/qr-scanner-cli
+ * convert: https://imagemagick.org/
+ * 
+ * /!\ zbarimg is not used as it doesn't work with the QR code on the swissqr,
+ * the documentation require to have a swiss flag in the middle of the QR code
+ * which trips zbarimg.
+ */
+define('QRAPP', '/usr/local/bin/qrscanner');
+define('IMGCONV', '/usr/bin/convert');
+define('DATADIR', './data/');
+/* Below 300dpi, the quality is not good enough so that it doesn't always detect
+ * the QR code.
+ */
+define('DENSITY', 300);
+/* DENSITY is in INCHES per PIXELS, so 300 DPI is 300/25.4 = 11.811023622 pixels 
+ * per mm 64mm is the size of the QR code according to swissqr documentation, 
+ * 64mm is the left position on the page and 206mm is the top position on the
+ * page.
+ */	
+define('RATIO', DENSITY / 25.4);
+/* https://www.six-group.com/dam/download/banking-services/standardization/qr-bill/style-guide-qr-bill-fr.pdf */
+
+// qrsize is 46mm + 5mm padding on each side
+define('QRSIZE', round(66 * RATIO));
+// must be 62mm from the left of the page
+define('XPOS', round(62 * RATIO));
+// 297mm, page height, - 95mm and 5mm.
+define('YPOS', round(197 * RATIO));
+
+$filename = $argv[1];
+$dir = DATADIR . '/' . md5($filename);
+mkdir($dir, 0777, true);
+
+echo $argv[1] . "\n";
+
+$rotate = 0;
+$found = false;
+/* Convert input file to png, crop to where the QR code must be according to
+ * swissqr documentation, try to get the qr code, rotate the image 90 degrees
+ * and try again until a qr code is found.
+ */
+do {
+	echo 'Rotate: ' . $rotate . "\n";
+	
+	exec(IMGCONV . ' -density ' . DENSITY . ' -colorspace gray -background white -alpha remove -alpha off -crop  ' . sprintf('%dx%d+%d+%d', QRSIZE, QRSIZE, XPOS, YPOS) . ' -rotate ' . $rotate . ' ' . $filename . ' ' . $dir . '/o.png');
+	$dh = opendir($dir);
+	if (!$dh) { break; }
+	while(($file = readdir($dh)) !== false) {
+		if ($file === '.' || $file === '..') { continue; }
+		echo 'File: ' . $file . "\n";
+		$output = [];
+		$rescode = 0;
+		exec(QRAPP . ' ' . $dir . '/' . $file . ' --clear 2>&1', $output, $rescode);
+		echo 'Rescode: ' . $rescode . "\n";
+		if ($rescode !== 0) { continue; }
+		$lastidx = count($output) - 1;
+
+		/* EPD must happend somewhere in the last 4 for lines */
+		if ($output[0] === 'SPC' 
+			&&
+			($output[$lastidx] === 'EPD'
+			|| $output[$lastidx - 1] === 'EPD'
+			|| $output[$lastidx - 2] === 'EPD'
+			|| $output[$lastidx - 3] === 'EPD')
+		)  {
+			$found = true;
+			break;
+		}
+
+	}
+	if($found) { break; }
+	$rotate += 90;
+} while ($rotate != 360);
+
+if(!$found) { return -1; }
+if (!verify_qrdata($output)) { return -1; }
+echo json_encode(bexio_from_qrdata($output), JSON_PRETTY_PRINT);
